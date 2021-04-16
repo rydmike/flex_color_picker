09d87ec54d866e4ec91fc6cb83c61c3320a35e25
Mike Rydstrom <mrydstrom+gh@gmail.com>
2021-04-16 21:07:51 +0300

    Upgrade packages and test build Win demo with master

--- example/lib/demo/screens/home/reset_settings_button.dart
+++ example/lib/demo/screens/home/reset_settings_button.dart
@@ -22,6 +22,12 @@ class ResetSettingsButton extends StatelessWidget {
           defaultActionText: translate.okButtonLabel,
           cancelActionText: translate.cancelButtonLabel,
         ).show(context, useRootNavigator: true);
+        // TODO: Potential lint rule false positive?
+        // Based on this: https://dart-lang.github.io/linter/lints/use_build_context_synchronously.html
+        // The rule should only apply to statefull widget. This Widget is
+        // Stateless, can't use proposed mounted check in a stateless widget!
+        // Thus we ignore the new experimental lint rule for now.
+        // ignore: use_build_context_synchronously
         if (didReset ?? false) resetSettings(context);
       },
       child: Padding(
