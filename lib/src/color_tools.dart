73fbe2fa8901e7cd0dd2e279623e022ef0074d38
rydmike <mrydstrom+gh@gmail.com>
2024-12-13 13:34:30 +0200

    ADD: Color extensions for deprecated 32bit and 8bit colors

--- lib/src/color_tools.dart
+++ lib/src/color_tools.dart
@@ -784,6 +784,50 @@ class ColorTools {
       _ColorName.fromColor(color).getName;
 }
 
+/// Public extensions on [Color].
+extension FlexColorPickerColorExtensions on Color {
+  /// A 32 bit value representing this color.
+  ///
+  /// This feature brings back the Color.value API in a way that is not and
+  /// will not be deprecated.
+  ///
+  /// The bits are assigned as follows:
+  ///
+  /// * Bits 24-31 are the alpha value.
+  /// * Bits 16-23 are the red value.
+  /// * Bits 8-15 are the green value.
+  /// * Bits 0-7 are the blue value.
+  int get value32bit {
+    return _floatToInt8(a) << 24 |
+        _floatToInt8(r) << 16 |
+        _floatToInt8(g) << 8 |
+        _floatToInt8(b) << 0;
+  }
+
+  /// The red channel of this color in an 8 bit value.
+  ///
+  /// This feature brings back the Color.red API in a way that is not and
+  /// will not be deprecated.
+  int get red8bit => (0x00ff0000 & value32bit) >> 16;
+
+  /// The green channel of this color in an 8 bit value.
+  ///
+  /// This feature brings back the Color.green API in a way that is not and
+  /// will not be deprecated.
+  int get green8bit => (0x0000ff00 & value32bit) >> 8;
+
+  /// The blue channel of this color in an 8 bit value.
+  ///
+  /// This feature brings back the Color.blue API in a way that is not and
+  /// will not be deprecated.
+  int get blue8bit => (0x000000ff & value32bit) >> 0;
+
+  // Convert float to 8 bit integer.
+  int _floatToInt8(double x) {
+    return (x * 255.0).round() & 0xff;
+  }
+}
+
 // Private class for storing and getting the name of a given color.
 // Use a color or string color code in hex format and get the closest color
 // name for it, from a list of colors that contains 1566 color names.
